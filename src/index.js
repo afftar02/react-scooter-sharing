a8f627a61b91a1bf474ce8a50ee2b75ea57fddee
Никита Стецкий <stets119@gmail.com>
2022-06-05 17:55:27 +0300

    double render removed

--- src/index.js
+++ src/index.js
@@ -49,9 +49,7 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 // );
 
 root.render(
-  <React.StrictMode>
-    <Router>
-      <App />
-    </Router>
-  </React.StrictMode>
+  <Router>
+    <App />
+  </Router>
 );
