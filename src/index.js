c69f73d96196ee27bb70c52d765258d254c65adc
Никита Стецкий <stets119@gmail.com>
2022-06-21 09:56:52 +0300

    Some redux basics added

--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom/client';
 import './index.scss';
 import App from './App';
 import { BrowserRouter as Router } from 'react-router-dom';
+import { store } from './redux/store';
+import { Provider } from 'react-redux';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
@@ -49,7 +51,9 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 // );
 
 root.render(
-  <Router>
-    <App />
-  </Router>
+  <Provider store={store}>
+    <Router>
+      <App />
+    </Router>
+  </Provider>
 );
